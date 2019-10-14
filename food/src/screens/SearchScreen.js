2a250b387c194d87a7d18937b8d6e51f2266a87d
Francis Niko Cruz <francisnikocruz@gmail.com>
2019-10-13 22:53:30 -0700

    initial commit

---
+++ food/src/screens/SearchScreen.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import SearchBar from '../components/SearchBar';
+import yelp from '../api/yelp';
+
+const SearchScreen =() =>{
+  const [term, setTerm] = useState(''); 
+  const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const searchApi = async (searchTerm) =>{
+    try{
+      const response = await yelp.get('/search',{
+        params:{
+          limit:50,
+          term:searchTerm,
+          location: 'union city'
+        }
+      });
+      setResults(response.data.businesses);
+    } catch (e){
+      setErrorMessage('Something went wrong');
+    }
+  };
+
+  useEffect(() =>{
+    searchApi('pasta');
+  }, []);
+
+  return(
+    <View>
+      <SearchBar 
+        term={term} 
+        onTermChange={setTerm}
+        onTermSubmit={() => searchApi(term)}
+      />
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
+      <Text>We have found {results.length} results</Text>
+    </View>
+  )
+};
+
+const style = StyleSheet.create({});
+
+export default SearchScreen;
