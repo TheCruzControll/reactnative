8d10a60a338fa7fe65b259c056f9441cf8781ae9
Francis Niko Cruz <francisnikocruz@gmail.com>
2019-10-17 00:37:34 -0700

    onto 3rd project

--- food/src/api/yelp.js
+++ food/src/api/yelp.js
@@ -1,8 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
 export default axios.create({
-  baseURL: 'https://api.yelp.com/v3/businesses',
-  headers:{
-    Authorization:'Bearer 3uQVqlg7G6EMh8KlT6SDUkalGjNW-OTnk6NOTZTPR_SUs14P4TSBW5upn-CdwUjmoOGxxVC53jV5gbvYWIBV6c1xZ19nGqjEIaacR6Um8PmNUPHWFl2lxFeC1NOjXXYx'
+  baseURL: "https://api.yelp.com/v3/businesses",
+  headers: {
+    Authorization:
+      "Bearer 3uQVqlg7G6EMh8KlT6SDUkalGjNW-OTnk6NOTZTPR_SUs14P4TSBW5upn-CdwUjmoOGxxVC53jV5gbvYWIBV6c1xZ19nGqjEIaacR6Um8PmNUPHWFl2lxFeC1NOjXXYx"
   }
 });
--- food/src/components/SearchBar.js
+++ food/src/components/SearchBar.js
@@ -1,41 +1,42 @@
-import React from 'react';
-import { View, TextInput, StyleSheet } from 'react-native';
-import { FontAwesome } from '@expo/vector-icons';
+import React from "react";
+import { View, TextInput, StyleSheet } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
 
-const SearchBar = ({term, onTermChange, onTermSubmit}) =>{
-  return(
+const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
+  return (
     <View style={styles.backgroundStyle}>
-      <FontAwesome name="search" style={styles.iconStyle}/>
-      <TextInput 
+      <FontAwesome name="search" style={styles.iconStyle} />
+      <TextInput
         placeholder="Search"
         style={styles.inputStyle}
         value={term}
         onChangeText={onTermChange}
-        autoCapitalize='none'
+        autoCapitalize="none"
         autoCorrect={false}
         onEndEditing={onTermSubmit}
       />
     </View>
-  )
+  );
 };
 
 const styles = StyleSheet.create({
-  backgroundStyle:{
-    backgroundColor:'#F0EEEE',
-    height:50,
-    borderRadius:10,
-    marginHorizontal:15,
-    flexDirection:'row',
-    marginTop:15
+  backgroundStyle: {
+    backgroundColor: "#F0EEEE",
+    height: 50,
+    borderRadius: 10,
+    marginHorizontal: 15,
+    flexDirection: "row",
+    marginTop: 15,
+    marginBottom: 15
   },
-  inputStyle:{
-    flex:1,
-    fontSize:18
+  inputStyle: {
+    flex: 1,
+    fontSize: 18
   },
-  iconStyle:{
-    fontSize:35,
-    alignSelf:'center',
-    marginHorizontal:15
+  iconStyle: {
+    fontSize: 35,
+    alignSelf: "center",
+    marginHorizontal: 15
   }
 });
 
